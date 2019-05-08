30b326f565b206a18d7f1798229fb6408aeb8c8a
NgocHNguyen <48600304+NgocHNguyen@users.noreply.github.com>
2019-05-08 23:34:19 +0700

    Add files via upload

---
+++ MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Pingpong
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void sign_in_bt_Click(object sender, EventArgs e)
+        {
+            SignIn aForm= new SignIn();
+            aForm.Show();
+        }
+
+        
+        
+    }
+}
---
+++ SignIn.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Pingpong
+{
+    partial class SignIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.passwd_tb = new System.Windows.Forms.TextBox();
+            this.user_nm_tb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.login_bt = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.login_bt);
+            this.panel1.Controls.Add(this.passwd_tb);
+            this.panel1.Controls.Add(this.user_nm_tb);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(371, 320);
+            this.panel1.TabIndex = 0;
+            // 
+            // passwd_tb
+            // 
+            this.passwd_tb.Location = new System.Drawing.Point(108, 144);
+            this.passwd_tb.Name = "passwd_tb";
+            this.passwd_tb.Size = new System.Drawing.Size(100, 20);
+            this.passwd_tb.TabIndex = 4;
+            // 
+            // user_nm_tb
+            // 
+            this.user_nm_tb.Location = new System.Drawing.Point(108, 83);
+            this.user_nm_tb.Name = "user_nm_tb";
+            this.user_nm_tb.Size = new System.Drawing.Size(100, 20);
+            this.user_nm_tb.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(91, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sign in";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(36, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User name";
+            // 
+            // login_bt
+            // 
+            this.login_bt.Location = new System.Drawing.Point(132, 205);
+            this.login_bt.Name = "login_bt";
+            this.login_bt.Size = new System.Drawing.Size(75, 23);
+            this.login_bt.TabIndex = 5;
+            this.login_bt.Text = "Log in";
+            this.login_bt.UseVisualStyleBackColor = true;
+            // 
+            // SignIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 320);
+            this.Controls.Add(this.panel1);
+            this.Name = "SignIn";
+            this.Text = "SignIn";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox passwd_tb;
+        private System.Windows.Forms.TextBox user_nm_tb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button login_bt;
+    }
+}
